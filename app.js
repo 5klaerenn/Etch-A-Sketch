375e4e1a5072b6436764f397c2b9b407b941b06b
B0bSnorkel <bob.snorkel@outlook.fr>
2022-08-06 17:35:06 -0400

    Refactored app.js to solve size changing problems Added basic functions to the grid (erase, reset, change size) Added grid template in JS

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-let num = 16;
-
-const gridCont = document.querySelector('#gridContainer');
-
-const gridReset = document.querySelector('#resetBtn');
-gridReset.addEventListener('click', () => { window.location.reload(); });
+//Grid basic functions 
 
 function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare');
-        gridCont.appendChild(gridSquare);
-        gridSquare.addEventListener('mouseenter', () => gridSquare.classList.add('hoveredIn'));
-        gridSquare.addEventListener('mouseenter', () => console.log(i + 1));
-    }
+        gridCont.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+        gridCont.style.gridTemplateRow = `repeat(${num}, 1fr)`;
+        gridCont.appendChild(gridSquare);       
+    } 
 
+    let gridPixels = gridCont.querySelectorAll('div');
+    gridPixels.forEach((el) => {
+        el.addEventListener('mouseenter', () => el.classList.add('hoveredIn'));
+        });
 }
 
+function eraseGrid(){
+    let gridPixels = gridCont.querySelectorAll('div');
+    gridPixels.forEach((el) => {
+        el.classList.remove('hoveredIn');
+        });
+}
+
+function resetGrid(){
+    gridCont.innerHTML = '';
+}
+
+function changeSize(){
+    num = prompt('How many squares per side for the new grid do you want for the new grid ?')
+    if(num >= 16 && num <= 100){
+        resetGrid();
+        createGrid(num);
+    }
+}
+
+//Add Color selection here
+
+//Declarations
+
+let num = 16;
+const gridCont = document.querySelector('#gridContainer');
+
+const eraseReset = document.querySelector('#eraseBtn');
+eraseReset.addEventListener('click', () => eraseGrid());
+
 const gridChange = document.querySelector('#changeGrid');
-gridChange.addEventListener('click', () => 
-    {num = prompt('How many squares per side for the new grid do you want for the new grid ?');
-    createGrid(num)});
-    
+gridChange.addEventListener('click', () => changeSize());
 
 createGrid(num)
-
-
