3b3002059dafbe0cd99cdbe9fdc54602ffdc0fd4
B0bSnorkel <bob.snorkel@outlook.fr>
2022-08-06 15:31:52 -0400

    Added function to change grid size based on user's answer

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+let num = 16;
+
 const gridCont = document.querySelector('#gridContainer');
 
 const gridReset = document.querySelector('#resetBtn');
-gridReset.addEventListener('click', () => {window.location.reload();});
+gridReset.addEventListener('click', () => { window.location.reload(); });
 
-
-
-function createGrid(num){
-    for (let i = 0; i < num*num ; i++){
-        let gridSquare = document.createElement('div'); 
+function createGrid(num) {
+    for (let i = 0; i < num * num; i++) {
+        let gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare');
         gridCont.appendChild(gridSquare);
         gridSquare.addEventListener('mouseenter', () => gridSquare.classList.add('hoveredIn'));
-        gridSquare.addEventListener('mouseenter', () => console.log(i+1));
-    }     
-    
-    
+        gridSquare.addEventListener('mouseenter', () => console.log(i + 1));
+    }
+
 }
 
+const gridChange = document.querySelector('#changeGrid');
+gridChange.addEventListener('click', () => 
+    {num = prompt('How many squares per side for the new grid do you want for the new grid ?');
+    createGrid(num)});
+    
 
-
-createGrid(16)
+createGrid(num)
 
 
